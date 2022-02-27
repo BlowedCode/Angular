ab9ce2a44b9f1c9ce2a28ba48feffdf317dfb92a
Michele Bartoli <blowedcode@gmail.com>
2022-02-27 16:27:55 +0100

    environment prod fix per build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBP9UsbIqJIRxtcORJ6gVhcbvZ8dSh5vTQ",
+    authDomain: "blowed-code.firebaseapp.com",
+    databaseURL: "https://blowed-code-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "blowed-code",
+    storageBucket: "blowed-code.appspot.com",
+    messagingSenderId: "64716051291",
+    appId: "1:64716051291:web:3e8e6cb12afddfe7a7407e"
+  }
 };
